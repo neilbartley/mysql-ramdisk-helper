2bc1c992f7f09b4da6e31badfeb5ce7544db48d6
Neil Bartley <n@neil.bar>
2017-04-03 10:39:24 +0100

    Added path option (e.g. for using keg versions from brew)

--- mysql-ramdisk-setup.sh
+++ mysql-ramdisk-setup.sh
@@ -10,10 +10,10 @@ RAMDISK_BLOCKS=$(($RAMDISK_SIZE_MB * 1048576 / 512))
 diskutil erasevolume HFS+ "mysql-ramdisk" `hdiutil attach -nomount ram://$RAMDISK_BLOCKS`
 
 echo ; echo "*** Initialising DB in ramdisk"
-mysqld --initialize \
-       --user mysql \
-       --basedir=/usr/local \
-       --datadir=$RAMDISK_MOUNT_POINT/mysql-ramdisk 2> /tmp/mysql-ramdisk-init.log ; cat /tmp/mysql-ramdisk-init.log
+$MYSQL_PATH/bin/mysqld --initialize \
+                       --user mysql \
+                       --basedir=/usr/local/opt/mysql@5.5 \
+                       --datadir=$RAMDISK_MOUNT_POINT/mysql-ramdisk 2> /tmp/mysql-ramdisk-init.log ; cat /tmp/mysql-ramdisk-init.log
 TEMP_PW=`cat /tmp/mysql-ramdisk-init.log | sed -n 's/.*A temporary password is generated for root@localhost: \(.*\)$/\1/p'` && rm /tmp/mysql-ramdisk-init.log
 
 if [[ -z TEMP_PW ]] ; then
@@ -22,13 +22,14 @@ if [[ -z TEMP_PW ]] ; then
 fi
 
 echo ; echo "*** Starting mysqld"
-mysqld --basedir=/usr/local \
-       --datadir=$RAMDISK_MOUNT_POINT/mysql-ramdisk \
-       --user=mysql --log-error=$RAMDISK_MOUNT_POINT/mysql-ramdisk/mysql.ramdisk.err \
-       --pid-file=/$RAMDISK_MOUNT_POINT/mysql-ramdisk/mysql.ramdisk.pid \
-       --port=3308 \
-       --socket=/tmp/mysql-ram.sock \
-       $MYSQLD_OPTIONS &
+$MYSQL_PATH/bin/mysqld --basedir=/usr/local/opt/mysql@5.5 \
+                       --datadir=$RAMDISK_MOUNT_POINT/mysql-ramdisk \
+                       --user=mysql \
+                       --log-error=$RAMDISK_MOUNT_POINT/mysql-ramdisk/mysql.ramdisk.err \
+                       --pid-file=/$RAMDISK_MOUNT_POINT/mysql-ramdisk/mysql.ramdisk.pid \
+                       --port=3308 \
+                       --socket=/tmp/mysql-ram.sock \
+                       $MYSQLD_OPTIONS &
 
 while [ ! -e $RAMDISK_MOUNT_POINT/mysql-ramdisk/mysql.ramdisk.pid ] ; do
   sleep 2
@@ -36,11 +37,11 @@ done
 echo "*** pid is:" `cat $RAMDISK_MOUNT_POINT/mysql-ramdisk/mysql.ramdisk.pid `
 
 echo ; echo "*** Changing mysql root password"
-mysql -uroot \
-      --socket=/tmp/mysql-ram.sock \
-      -p$TEMP_PW \
-      --connect-expired-password \
-      -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+$MYSQL_PATH/bin/mysql -uroot \
+                      --socket=/tmp/mysql-ram.sock \
+                      -p$TEMP_PW \
+                      --connect-expired-password \
+                      -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
 
 unset TEMP_PW
 
